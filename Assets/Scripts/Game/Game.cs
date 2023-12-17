e6cff2a90c936dfe2a7713155e289c746493d1eb
LeChiffre2110 <younes.abdelwadoud@outlook.de>
2023-12-17 15:57:00 +0100

    feature: add basic logic and define interface

--- Assets/Scripts/Game/Game.cs
+++ Assets/Scripts/Game/Game.cs
@@ -1,5 +1,80 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Text;
 
+//TODO: remove magic numbers
 public class Game : IGame
 {
+    private string _currentSequence;
+    private string _currentGameMode;
 
+    public void StartGame(string gameMode, IBoard board)
+    {
+        _currentGameMode = gameMode;
+        _currentSequence = GenerateSequence(10);
+        Debug.Log(_currentSequence);
+        board.SendMessageToBoard(_currentSequence);
+    }
+
+    public string GetCurrentSequence()
+    {
+        return _currentSequence;
+    }
+
+    public void StartGameFromSequence(string sequence)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public void EndGame()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public void PauseGame()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public void ResumeGame()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public void RestartGame()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public void QuitGame()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    private string GenerateSequence(int sequenceLength)
+    {
+        StringBuilder sequenceBuilder = new StringBuilder();
+        for (int i = 0; i < sequenceLength; i++)
+        {
+            if (i == 0 || i == 1)
+            {
+                sequenceBuilder.Append(HoldExtensions.GetRandomHold());
+                sequenceBuilder.Append(HoldInstructionExtensions.GetStartHoldInstruction());
+            }
+            else if (i == sequenceLength - 1 || i == sequenceLength - 2)
+            {
+                sequenceBuilder.Append(HoldExtensions.GetRandomHold());
+                sequenceBuilder.Append(HoldInstructionExtensions.GetEndHoldInstruction());
+            }
+            else
+            {
+                sequenceBuilder.Append(HoldExtensions.GetRandomHold());
+                sequenceBuilder.Append(HoldInstructionExtensions.GetRandomHoldInstruction());
+            }
+        }
+        string sequence = sequenceBuilder.ToString();
+        Debug.Log(sequence);
+        return sequence;
+    }
 }
--- Assets/Scripts/Game/IGame.cs
+++ Assets/Scripts/Game/IGame.cs
@@ -1,4 +1,13 @@
 public interface IGame 
 {
+    void StartGame(string gameMode, IBoard board); //TODO: revert back after testing
 
+    void StartGameFromSequence(string sequence);
+    void EndGame();
+    void PauseGame();
+    void ResumeGame();
+    void RestartGame();
+    void QuitGame();
+    //ONLY FOR TESTING
+    string GetCurrentSequence();
 } 
