274f59ae52e375c7c011177303aab462f0e66da2
LeChiffre2110 <younes.abdelwadoud@outlook.de>
2023-12-17 15:54:51 +0100

    Refactor: implement test controller logic

--- Assets/Scripts/Controller/Controller.cs
+++ Assets/Scripts/Controller/Controller.cs
@@ -1,4 +1,28 @@
-public class Controller 
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Controller : MonoBehaviour
 {
-    
+    private IBoard _board;
+    private IGame _game;
+
+    void Start()
+    {
+        _board = new Board();
+        _game = new Game();
+
+        _board.ConnectToBoard();
+        _game.StartGame("normal", _board);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    void OnApplicationQuit()
+    {
+        _board.DisconnectFromBoard();
+    }
 }
