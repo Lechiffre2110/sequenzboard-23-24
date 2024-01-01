a0164502b8c8a32663f3829186f916cdae3d39c9
LeChiffre2110 <younes.abdelwadoud@outlook.de>
2024-01-01 20:05:42 +0100

    feature: add game starting logic

--- Assets/Scripts/Controller/Controller.cs
+++ Assets/Scripts/Controller/Controller.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class Controller : MonoBehaviour
 {
     private IGame _game;
+    [SerializeField] private UserInterface _userInterface;
+
 
     void Start()
     {
@@ -14,11 +16,17 @@ public class Controller : MonoBehaviour
 
     void FixedUpdate()
     {
-        _game.StartGame("normal");
+        //_game.StartGame("normal");
     }
 
     void OnApplicationQuit()
     {
         _game.QuitGame();
+    }
+
+    public void StartGameWithRandomSequence()
+    {
+        _game.StartGame("normal");
+        _userInterface.PlaySequence(_game.GetCurrentSequence());
     }
 }
