c68b9c6dc99aa613bdb14c85c93104919aeb5c01
LeChiffre2110 <younes.abdelwadoud@outlook.de>
2024-01-29 21:38:59 +0100

    feature: add mongodb saving functionality

--- Assets/Scripts/Data/Data.cs
+++ Assets/Scripts/Data/Data.cs
@@ -1,27 +1,71 @@
-public class Data : IData
-{
-    public void SaveSequence()
-    {
-        throw new System.NotImplementedException();
-    }
+using System;
+using System.Runtime.ConstrainedExecution;
+using UnityEngine;
+using MongoDB.Bson;
+using MongoDB.Driver;
+using System.Collections.Generic;
+using System.Linq;
 
-    public void GetSequence()
+public class Data
     {
-        throw new System.NotImplementedException();
-    }
+        private string connectionUrl = "mongodb://localhost:27017/?directConnection=true&serverSelectionTimeoutMS=2000&appName=mongosh+1.6.1";
+        private string dbName = "SequenzboardDB";
+        private string collectionName = "Sequenzen";
 
-    public void GetAllSequences()
-    {
-        throw new System.NotImplementedException();
-    }
+        public List<SequenceModel> GetAllSequences()
+        {
+            //Establish db connection
+            var client = new MongoClient(connectionUrl);
+            var db = client.GetDatabase(dbName);
+            var sequenceCollection = db.GetCollection<SequenceModel>(collectionName);
 
-    public void UpdateSequence()
-    {
-        throw new System.NotImplementedException();
-    }
+            var results = sequenceCollection.Find(_ => true);
 
-    public void DeleteSequence()
-    {
-        throw new System.NotImplementedException();
+            List<SequenceModel> allSequences = new List<SequenceModel>();
+
+            foreach (var result in results.ToList())
+            {
+                allSequences.Add(result);
+            }
+
+            return allSequences;
+        }
+
+        public void SaveSequence(string name, string sequence)
+        {
+            Debug.Log("SaveSequence: " + name + " " + sequence);
+            try {
+                //Establish db connection
+                var client = new MongoClient(connectionUrl);
+                var db = client.GetDatabase(dbName);
+                var sequenceCollection = db.GetCollection<SequenceModel>(collectionName);
+
+                var document = new SequenceModel
+                {
+                    name = name,
+                    sequence = sequence
+                };
+
+                sequenceCollection.InsertOne(document);
+                Debug.Log("Saved sequence: " + name + " " + sequence);
+            } catch (Exception e) {
+                Debug.Log("Error saving sequence: " + e);
+            }
+        }
+
+        public void DeleteSequence(string name)
+        {
+            //Establish db connection
+            var client = new MongoClient(connectionUrl);
+            var db = client.GetDatabase(dbName);
+            var sequenceCollection = db.GetCollection<SequenceModel>(collectionName);
+
+            var filter = Builders<SequenceModel>.Filter.Eq("name", name);
+
+            sequenceCollection.DeleteOne(filter);
+        }
     }
-}
+    
+
+        
+        
